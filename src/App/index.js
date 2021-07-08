2827c96c0d716b506dcbcf04bd62685e2f15c601
hauteballe <hauteballe@gmail.com>
2021-07-08 17:20:17 +0300

    refactor

---
+++ src/App/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import styles from './styles.module.css';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+
+const App = () => {
+  return (
+    <div className={styles.wrapper}>
+<<<<<<< HEAD:src/App/App.js
+      <Header user='Ирина' />
+      <Main />
+      <Footer country='Беларусь' />
+=======
+      <Header user="Ирина" />
+      <Main/>
+      <Footer country="Беларусь"/>
+>>>>>>> feature/bug-fix:src/App/index.js
+    </div>
+  );
+};
+
+export default App;
