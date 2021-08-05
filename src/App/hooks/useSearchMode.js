1de30fea1ca5fd7846f0ef0582857a2f29a59b1f
hauteballe <hauteballe@gmail.com>
2021-08-05 21:52:32 +0300

    fix: implement correct searching with uppercase

--- src/App/hooks/useSearchMode.js
+++ src/App/hooks/useSearchMode.js
@@ -37,7 +37,7 @@ const useSearchMode = () => {
 
   const search = (text) => {
     setSearchText(text);
-    if (text === 'javascript') {
+    if (text.toLowerCase() === 'javascript') {
       setSearchResult(jsSearchResult);
     } else {
       setSearchResult([]);
